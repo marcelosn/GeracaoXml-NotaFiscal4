272dc3e80d872a7afe7500e98886b19f81a2ba55
Matheus Sampaio <sampaio.brenner@gmail.com>
2018-06-05 22:11:44 -0300

    Teste de passagem de valores para escrita do xml

--- Nfe/Program.cs
+++ Nfe/Program.cs
@@ -8,8 +8,20 @@ namespace Nfe
         {
             var item = new enviNFeNFeInfNFeDetProd { cProd = 1, CFOP = 5405, qCom = 1 };
 
-            var cabecalho = new enviNFe { versao = 4 };
-            var xml = GerarXml<enviNFe>.Serialize(cabecalho);
+            var nota =
+                new enviNFe
+                {
+                    versao = 4,
+                    NFe = new enviNFeNFe
+                    {
+                        infNFe =
+                                new enviNFeNFeInfNFe
+                                {
+                                    dest = new enviNFeNFeInfNFeDest { xNome = "Matheus", email = "teste@teste.com" }
+                                }
+                    }
+                };
+            var xml = GerarXml<enviNFe>.Serialize(nota);
 
             Console.WriteLine(xml);
             Console.ReadLine();
